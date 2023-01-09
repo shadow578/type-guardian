4bd1a55c551cfe454ba4b7c40c355eeb7bde1d4f
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-01-09 18:52:14 +0100

    export all functions from `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-//TODO: implementation
+// Matchers.ts
+import { match, matchUnion, matchNullable, matchArray, matchEnumeration, matchObject } from './Matchers';
+export { match, matchUnion, matchNullable, matchArray, matchEnumeration, matchObject };
+
+// TypeGuard.ts
+import { makeTypeGuard } from './TypeGuard';
+export { makeTypeGuard };
